0c0ae1c112147f18da09a4660d09242db4cec632
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-08-21 10:52:44 -0500

    Working on DFU Firmware

--- Sources/NordicDFU/ManifestFirmwareInfo.swift
+++ Sources/NordicDFU/ManifestFirmwareInfo.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+#if swift(>=3.2)
+#elseif swift(>=3.0)
+    import Codable
+#endif
+
+public struct DFUManifestFirmwareInfo {
+    
+    public var binFile: String
+    
+    public var datFile: String?
+}
+
+// MARK: - Codable
+
+extension DFUSoftdeviceBootloaderInfo: Codable {
+    
+    public enum CodingKeys: String, CodingKey {
+        
+        case binFile = "bin_file"
+        case datFile = "dat_file"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.binFile = try container.decode(String.self, forKey: .binFile)
+        self.datFile = try container.decodeIfPresent(String.self, forKey: .datFile)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(binFile, forKey: .binFile)
+        try container.encode(datFile, forKey: .datFile)
+    }
+}
