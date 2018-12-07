6149cb6adc983db373ca273c829fda8d962ac133
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-12-06 21:53:25 -0500

    Updated `DFUSoftdeviceBootloaderInfo`

--- Sources/NordicDFU/SoftdeviceBootloaderInfo.swift
+++ Sources/NordicDFU/SoftdeviceBootloaderInfo.swift
@@ -9,7 +9,15 @@
 import Foundation
 
 /// Nordic DFU Softdevice Bootloader Information
-public struct DFUSoftdeviceBootloaderInfo: DFUManifestFirmwareInfoProtocol {
+public struct DFUSoftdeviceBootloaderInfo: DFUManifestFirmwareInfoProtocol, Codable, Equatable {
+    
+    internal enum CodingKeys: String, CodingKey {
+        
+        case binFile = "bin_file"
+        case datFile = "dat_file"
+        case bootloaderSize = "bl_size"
+        case softdeviceSize = "sd_size"
+    }
     
     public var binFile: String
     
@@ -18,39 +26,4 @@ public struct DFUSoftdeviceBootloaderInfo: DFUManifestFirmwareInfoProtocol {
     public var bootloaderSize: UInt32?
     
     public var softdeviceSize: UInt32?
-}
-
-// MARK: - Codable
-
-extension DFUSoftdeviceBootloaderInfo: Codable {
-    
-    public enum CodingKeys: String, CodingKey {
-        
-        case binFile = "bin_file"
-        case datFile = "dat_file"
-        case bootloaderSize = "bl_size"
-        case softdeviceSize = "sd_size"
-    }
-    
-    public init(from decoder: Decoder) throws {
-        
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        self.binFile = try container.decode(String.self, forKey: .binFile)
-        self.datFile = try container.decodeIfPresent(String.self, forKey: .datFile)
-        
-        self.bootloaderSize = try container.decodeIfPresent(UInt32.self, forKey: .bootloaderSize)
-        self.softdeviceSize = try container.decodeIfPresent(UInt32.self, forKey: .softdeviceSize)
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(binFile, forKey: .binFile)
-        try container.encode(datFile, forKey: .datFile)
-        
-        try container.encode(bootloaderSize, forKey: .bootloaderSize)
-        try container.encode(softdeviceSize, forKey: .softdeviceSize)
-    }
 }
