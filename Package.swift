44326cdb400cfc9c1d89d158b8e9db68dfea3b44
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-04-12 23:41:48 -0500

    Build as dynamic library on Linux

--- Package.swift
+++ Package.swift
@@ -1,14 +1,24 @@
 // swift-tools-version:5.1
 import PackageDescription
 
+#if os(Linux)
+let libraryType: PackageDescription.Product.Library.LibraryType = .dynamic
+#else
+let libraryType: PackageDescription.Product.Library.LibraryType = .static
+#endif
+
 let package = Package(
     name: "NordicDFU",
     platforms: [
-        .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_11),
+        .iOS(.v9),
+        .tvOS(.v9),
+        .watchOS(.v2)
     ],
     products: [
         .library(
             name: "NordicDFU",
+            type: libraryType,
             targets: ["NordicDFU"]
         ),
         .executable(
